5dfc555ec953e6dbe1392d8b414ae85f820db122
Markus Kalkbrenner <markus.kalkbrenner@bio.logis.de>
2020-09-28 17:08:01 +0200

    avoid frequent switch events

--- Star-Trek-TNG-PUPPack/Star-Trek-TNG-PUPPack.ino
+++ Star-Trek-TNG-PUPPack/Star-Trek-TNG-PUPPack.ino
@@ -3,15 +3,20 @@
 //Interfaced for pinduino shield TNG (Mega Pro Embed!)
 //Uses pinduino library WIP
 
-#include <pinduino.h>
+#include "pinduino.h"
 
 pinduino pd("TNG");
 
 void setup() {
     Serial.begin(115200);
-    pd.pin2Dmd()->forward(pd.pupSerial);
+    pd.pin2Dmd()->forwardToPUP(pd.pupSerial());
 }
 
 void loop() {
     pd.pin2Dmd()->update();
+
+    if (pd.switchMatrix()->getOnce(/* row */ 2, /* column */ 1, /* ignore for further reads for milli seconds */ 200)) {
+        pd.pupSerial()->postEvent(PUP_TYPE_SWITCH, /* switch number */ 12);
+        pd.switchMatrix()->reset();
+    }
 }
